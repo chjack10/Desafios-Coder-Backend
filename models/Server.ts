aa1b15a5541c146083170b18851e7fb476e551c6
chjack10 <christianyak@gmail.com>
2022-06-07 00:29:48 -0300

    tmp merged

--- models/Server.ts
+++ models/Server.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import productRoutes from '../routes/product';
 import cartRoutes from '../routes/cart';
 import defaultRoutes from '../routes/default';
-// import auth from '../middlewares/auth';
+import auth from '../middlewares/auth';
 import { Server as HttpServer } from 'http';
 import { Server as IOServer } from 'socket.io';
 
@@ -38,8 +38,8 @@ class Server {
         extended: true,
       })
     );
-    // this.app.use(auth);
-    this.app.use(express.static('public'));
+    this.app.use(auth);
+    // this.app.use(express.static('public'));
   }
 
   routes() {
@@ -49,34 +49,34 @@ class Server {
     this.app.use(this.apiPaths.default, defaultRoutes);
   }
 
-  // listen() {
-  //   this.app.listen(this.port, () => {
-  //     console.log('Server running on port', this.port);
-  //   });
-  // }
-
   listen() {
-    this.httpServer.listen(this.port, () => {
+    this.app.listen(this.port, () => {
       console.log('Server running on port', this.port);
     });
-
-    this.ioServer.on('connection', async (socket) => {
-      console.log(`Client ${socket.id} connected`);
-
-      socket.emit('products', await products.getAll());
-      socket.emit('chatMessages', await chat.getAllMessages());
-
-      socket.on('newProduct', async (product) => {
-        products.add(product);
-        this.ioServer.sockets.emit('products', await products.getAll());
-      });
-
-      socket.on('newChatMessage', async (newMessage) => {
-        chat.addMessage(newMessage);
-        this.ioServer.sockets.emit('chatMessages', await chat.getAllMessages());
-      });
-    });
   }
+
+  // listen() {
+  //   this.httpServer.listen(this.port, () => {
+  //     console.log('Server running on port', this.port);
+  //   });
+
+  //   this.ioServer.on('connection', async (socket) => {
+  //     console.log(`Client ${socket.id} connected`);
+
+  //     socket.emit('products', await products.getAll());
+  //     socket.emit('chatMessages', await chat.getAllMessages());
+
+  //     socket.on('newProduct', async (product) => {
+  //       products.add(product);
+  //       this.ioServer.sockets.emit('products', await products.getAll());
+  //     });
+
+  //     socket.on('newChatMessage', async (newMessage) => {
+  //       chat.addMessage(newMessage);
+  //       this.ioServer.sockets.emit('chatMessages', await chat.getAllMessages());
+  //     });
+  //   });
+  // }
 }
 
 export default new Server();
